8e9f2ee3767d0a87da71e8ebde0a42a008f1d006
ratulbasak <ratulbasak93@gmail.com>
2021-12-31 05:01:52 +0600

    added command and groups

--- ksns/cli.py
+++ ksns/cli.py
@@ -1,11 +1,18 @@
-import click
+import click, yaml
+from click.decorators import help_option
 
 from ksns.client import K8s
 from ksns.helpers import get_kubeconfig
 from ksns.const import homedir, env_kubeconfig, path_kubeconfig
 
-@click.command()
+@click.group()
 def main():
+    pass
+
+
+@main.command()
+def list():
+    """ List of namespaces in context """
     if env_kubeconfig is None:
         kubeconfig = homedir + path_kubeconfig
     else:
@@ -21,6 +28,33 @@ def main():
         else: 
             fg = ''
         click.secho(namespace, fg=fg, nl=True)
+
+@main.command()
+@click.argument('namespace')
+def ns(namespace):
+    """ Switch to another namespace: <namespace_name> """
+    namespace_arr = []
+    if env_kubeconfig is None:
+        kubeconfig = homedir + path_kubeconfig
+    else:
+        kubeconfig = env_kubeconfig
+    kctx = K8s(configuration_yaml=kubeconfig)
+    list_namespaces = kctx.client.list_namespace(watch=False)
+    for i in list_namespaces.items:
+        namespace_arr.append(i.metadata.name)
+    if namespace not in namespace_arr:
+        print(f'namespace {namespace} not found')
+    current_namespace = kctx.config['contexts'][0]['context']['namespace']
+    with open(kubeconfig) as file:
+        getconfig = yaml.load(file, Loader=yaml.FullLoader)
+
+    with open(kubeconfig, 'w') as config_file:
+        getconfig['contexts'][0]['context']['namespace'] = namespace
+        sort_file = yaml.dump(getconfig, config_file, sort_keys=True)
+    
+    click.secho(f"switched to {namespace} namespace", fg='green', nl=True)
+        
+    
 
 if __name__ == "__main__":
     main()
